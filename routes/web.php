50c2afc6a914a3277bd634d3071ef08a7339a3f7
chaymaoualili-dotcom <chayma.oualili@etu.uae.ac.ma>
2026-01-12 15:51:10 +0100

    Revert "fronted"
    This reverts commit 05a5b8314a342277ff42c2b0bc35729cd8b81ecb.

--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test-design', function () {
-    return view('test');
-});
-
-// Route temporaire pour éviter l'erreur "logout not defined"
-Route::post('/logout', function () {
-    return "Déconnexion (Test)";
-})->name('logout');
-
-Route::get('/logout', function () {
-    return "Déconnexion (Test)";
-})->name('logout.get');
-
-
-Route::get('/test-login', function () {
-    return view('test-login');
-});
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-// routes/web.php
-
-use App\Http\Controllers\AuthController; // Vérifie que c'est bien importé
-
-// Page de demande de compte
-Route::get('/demande-compte', [AuthController::class, 'showAccountRequest'])->name('account.request');
-Route::post('/demande-compte', [AuthController::class, 'submitAccountRequest'])->name('account.request.submit');
